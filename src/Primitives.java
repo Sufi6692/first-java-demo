cbda1c1e20b675c3bfa2fa0ba028f445730116ed
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-06 21:41:24 +0530

    Understand what is mean by Primitive and Non-Primitive data types

--- src/Primitives.java
+++ src/Primitives.java
@@ -2,17 +2,19 @@ public class Primitives {
     public static void main(String[] args) {
 
 
-//        int rollno = 64;
+//        int rollNo = 64;
 //        char letter = 's';
 //        float marks = 98.45f;
 //        double largeDecimalNumbers = 444544.4554;
 //        long largeInteger = 565646516546L;
 //        boolean check = false;
 
-
+// Check what is primitive data types in chatGPT
 
         //String is not primitive
-        String name = "sufi";
+//        String name = "sufi";
+
+
 
 
     }
