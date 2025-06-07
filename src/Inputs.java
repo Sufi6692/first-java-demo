4440af12275e25333bfc372f4386e34a8d57c8a2
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-06-07 18:19:24 +0530

    Understand about the Inputs

---
+++ src/Inputs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+//        int rollNo = input.nextInt();
+//        System.out.println("Your roll number is " + rollNo);
+
+        int a = 10;
+        String name = input.nextLine();
+        System.out.println(name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+
+    }
+}
